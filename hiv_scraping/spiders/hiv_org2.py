7a78133b01f7b3ac27f4a9eb3b8520ec28f7846f
Luis Akakpo <luis.akakpo@gmail.com>
2018-02-21 10:52:14 +0100

    multiple sites

--- hiv_scraping/spiders/hiv_org2.py
+++ hiv_scraping/spiders/hiv_org2.py
@@ -7,18 +7,19 @@ class HivOrg2Spider(scrapy.Spider):
     name = 'hiv_orgs'
     allowed_domains = ['www.nacosa.org.za', 'www.aids.org.za','hivsa.com','www.caprisa.org']
     start_urls = ['http://www.nacosa.org.za/','https://www.aids.org.za' ,'http://hivsa.com/','http://www.caprisa.org/Default']
-    saved_links = []
+    saved_domains = []
     dead_ends = {}
     restricted_sections = []
 
     def parse(self, response):
-        links = LinkExtractor(allow=(), deny= self.allowed_domains + self.saved_links).extract_links(response)
+        links = LinkExtractor(allow=(), deny= self.allowed_domains + self.saved_domains).extract_links(response)
 
         for link in links:
-            self.saved_links.append(link.url)
+            self.saved_domains.append(self.stripurl(link.url))
 
             yield {'link': link.url,
-                   'domain': self.stripurl(link.url)}
+                   'domain': self.stripurl(link.url),
+                   'referer' : self.stripurl(response.request.url)}
 
 
         next_links = LinkExtractor(allow= self.allowed_domains, deny = self.restricted_sections).extract_links(response)
