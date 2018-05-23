e28a8972bb6fdc9382bc9760c865c6886d8bc448
Luis Akakpo <luis.akakpo@gmail.com>
2018-05-23 10:54:26 +0200

    [ENH] Improved version

--- website labeling.py
+++ website labeling.py
@@ -8,7 +8,7 @@
 # 
 # Load a new URL into a previously created WebView window. This function must be invoked after WebView windows is created with create_window(). Otherwise an exception is thrown.
 
-# In[ ]:
+# In[1]:
 
 
 import threading
@@ -25,50 +25,43 @@ class webThread(threading.Thread):
         try :
             # fetch most recent version
             d = pd.read_csv('dom_lbl.csv')
-            self.url_list = d[d['hiv'].isnull()]['domain'][:limit].tolist()
-            self.past_df = d[d['hiv'].notnull()]
-
+            self.urls = d[d['hiv'].isnull()]['domain'][:limit]
+            self.past_df = d.copy()
+        
         except :
             # start from 0
             d = pd.read_csv('domains.csv')
-            self.url_list = d['domain'][:limit].tolist()
-            self.past_df = None
-
-
+            self.urls = d['domain'][:limit]
+            self.past_df = d.copy()
 
         self.lbls = ['hiv','health','gov','edu','ngo']
         # init dataframe
-        self.data_lbl = d
+        self.data_lbl = d.copy()
         for lbl in self.lbls :
             self.data_lbl[lbl] = None
         
     def run(self):
-        idx=0
-        for url in self.url_list :
-            
+        for idx,url in self.urls.iteritems() :
             print "Starting " + self.name + " on " + url
-
             webview.load_url(url)
-            
             print 'Getting input...'
             for lbl in self.lbls :
                 while True : #ask for input while the user makes mistakes
                     try :
                         rep = input(lbl + '?')
+                        if rep not in [0,1]:
+                            raise
                         self.data_lbl.loc[idx,lbl] = rep
                     except :
                         print "Please enter 0 or 1"
                     else :
                         break
-            idx+=1
-            if self.past_df is None:
-                self.data_lbl.to_csv('dom_lbl.csv', index=False)
-            else :
-                pd.concat([self.past_df,self.data_lbl])                   .to_csv('dom_lbl.csv', index=False)
-            
+
+            self.past_df.update(self.data_lbl, overwrite = False)
+            self.past_df.to_csv('dom_lbl.csv', index=False)
 
 # Create new threads
-web_thread = webThread(1, "Web Thread", 2)
+web_thread = webThread(1, "Web Thread")
 
 # Start new Threads
 web_thread.start()
@@ -76,17 +69,35 @@ web_thread.start()
 webview.create_window('Please label this site', width=1200, height=800, resizable=True, fullscreen=False)
 
 
-# In[ ]:
-
-
-import pandas as pd
-# fetch most recent version
-d = pd.read_csv('dom_lbl.csv')
-
-
+# # Quick labeling of recuring websites
 
 # In[ ]:
 
 
-d[d['hiv'].notnull()]
+# import pandas as pd
+# d = pd.read_csv('domains.csv')
+# lbls = ['hiv','health','gov','edu','ngo']
+# # init dataframe
+# for lbl in lbls :
+#     d[lbl] = None
+
+
+# In[ ]:
+
+
+# for lbl in lbls :
+#     d.loc[d['domain'].str.contains('.iu.edu'),lbl] = 0
+# d.loc[d['domain'].str.contains('.iu.edu'),'edu'] = 1
+
+
+# In[ ]:
+
+
+# d.to_csv('dom_lbl.csv',index=False)
+
+
+# In[ ]:
+
+
+# d
 
